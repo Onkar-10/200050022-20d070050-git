ab6804772a82a777defdf7c05d2237e55a58acdc
Onkar-10 <onkarborade1@gmail.com>
2021-08-07 00:44:55 +0530

    feat: Login API

--- main.cpp
+++ main.cpp
@@ -5,9 +5,7 @@ using namespace std;
 int main()
 {
     string name1,password1;
-    cout<<"Enter Name:- ";
     cin>>name1;
-    cout<<"Enter password:- ";
     cin>>password1;
     if(login(name1,password1))
     cout<<"Success!";
